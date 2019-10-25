ebad5246c947f6728cd418df29f2a0488abb5eb1
unknown <fegoworks@hotmail.com>
2019-10-25 13:31:39 +0100

    chore: install and set up mongoose

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
+// password pYiGhIcKLjKBB1fh
+// conncection mongodb+srv://feggie:<password>@cluster0-lbsd8.mongodb.net/test?retryWrites=true&w=majority
+
 const express = require('express')
+const bodyparser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express();
 
-// app.use((req, res) => {
-//   res.json({
-//     message: "Request successful"
-//   })
-// })
+mongoose.connect('mongodb+srv://feggie:pYiGhIcKLjKBB1fh@cluster0-lbsd8.mongodb.net/test?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('successfully connected to MongoDB Atlas!')
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.log(error)
+  })
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+
+app.use(bodyparser.json())
+
+app.post('/api/recipes', (req, res, next) => {
+  console.log(req.body)
+  res.status(201).json({
+    message: "Added successfully"
+  })
+})
 
 app.use('/api/recipes', (req, res, next) => {
   const recipes = [{
