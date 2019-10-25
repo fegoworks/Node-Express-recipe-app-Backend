18e617b30d84e5037b7d0b86a65120447a5b8075
unknown <fegoworks@hotmail.com>
2019-10-25 14:00:16 +0100

    feature(endpoints): build endpoints

--- app.js
+++ app.js
@@ -4,10 +4,14 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const mongoose = require('mongoose')
+const Recipe = require('./models/recipe');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://feggie:pYiGhIcKLjKBB1fh@cluster0-lbsd8.mongodb.net/test?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://feggie:pYiGhIcKLjKBB1fh@cluster0-lbsd8.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => {
     console.log('successfully connected to MongoDB Atlas!')
   })
@@ -26,30 +30,98 @@ app.use((req, res, next) => {
 app.use(bodyparser.json())
 
 app.post('/api/recipes', (req, res, next) => {
-  console.log(req.body)
-  res.status(201).json({
-    message: "Added successfully"
-  })
-})
+  const {
+    title,
+    ingredients,
+    instructions,
+    difficulty,
+    time
+  } = req.body;
+  const recipe = new Recipe({
+    title: title,
+    ingredients: ingredients,
+    instructions: instructions,
+    difficulty: difficulty,
+    time: time,
+  });
+  recipe.save().then(() => {
+    res.status(201).json({
+      message: 'Post saved successfully'
+    });
+  }).catch((error) => {
+    res.status(400).json({
+      error: error
+    });
+  });
+});
+
+app.get('/api/recipes/:id', (req, res, next) => {
+
+  Recipe.findOne({
+    _id: req.params.id
+  }).then(
+    (recipe) => {
+      res.status(200).json(recipe);
+    }
+  ).catch((error) => {
+    res.status(400).json({
+      error: error
+    });
+  });
+});
+
+app.put('/api/recipes/:id', (req, res, next) => {
+  const {
+    title,
+    ingredients,
+    instructions,
+    difficulty,
+    time
+  } = req.body;
+  const recipe = new Recipe({
+    _id: req.params.id,
+    title: title,
+    ingredients: ingredients,
+    instructions: instructions,
+    difficulty: difficulty,
+    time: time,
+  });
+  Recipe.update({
+    _id: req.params.id
+  }, recipe).then(() => {
+    res.status(201).json({
+      message: 'Post saved successfully'
+    });
+  }).catch((error) => {
+    res.status(400).json({
+      error: error
+    });
+  });
+});
+
+app.delete('/api/recipes/:id', (req, res, next) => {
+  Recipe.deleteOne({
+    _id: req.params.id
+  }).then(() => {
+    res.status(200).json({
+      message: 'Deleted'
+    });
+
+  }).catch((error) => {
+    res.status(400).json({
+      error: error
+    });
+  });
+});
 
 app.use('/api/recipes', (req, res, next) => {
-  const recipes = [{
-    _id: 'sdhksfhfb',
-    title: 'awesome meal',
-    ingredients: 'we in the house like 30 minutes',
-    instructions: 'lorem ipsum east all the rats, secure the mouse',
-    difficulty: 10,
-    time: 3,
-  }, {
-    _id: 'sdhksfhfbas',
-    title: 'delicious meal',
-    ingredients: 'we in the house like 30 minutes',
-    instructions: 'lorem ipsum east all the rats, secure the mouse',
-    difficulty: 50,
-    time: 3,
-  }, ];
-
-  res.status(200).json(recipes)
+  Recipe.find().then((recipes) => {
+    res.status(200).json(recipes);
+  }).catch((error) => {
+    res.status(400).json({
+      error: error
+    });
+  });
 });
 
 module.exports = app;
